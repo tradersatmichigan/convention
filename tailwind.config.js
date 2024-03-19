b390bf5a9ba864b5b9f973b2e67806b449868586
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-03-18 22:24:02 -0400

    move html to docs for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   mode: 'jit',
   theme: {
     extend: {},
